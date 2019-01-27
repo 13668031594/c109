e319e37e2325be14da5d9faf7570364f5ef10f5a
yangyang <450428985@qq.com>
2019-01-27 15:17:16 +0800

    123

--- app/Http/Classes/Index/Order/SellClass.php
+++ app/Http/Classes/Index/Order/SellClass.php
@@ -46,9 +46,25 @@ class SellClass extends IndexClass
         $other = [
             'where' => $where,
             'orderBy' => $order,
+            'select' => [
+                'id','young_buy_order as buyCode','young_sell_order as sellCode','young_total as amount','created_at',
+                'young_status','young_buy_nickname as to','young_pay_time as payTime','young_bank_name as bankName',
+                'young_bank_no as bankNo','young_bank_address as bankAddress','young_bank_man as bankUser','young_alipay',
+                'young_note as bankNote','young_sell_nickname as payee','young_buy_uid'
+            ],
         ];
 
-        return parent::list_all('match_order', $other);
+        $member = parent::get_member();
+
+        $result = parent::list_all('match_order', $other);
+        foreach ($result as &$v){
+
+            $v['toReferee'] = MemberModel::whereUid($v['buy_uid'])->first()->young_referee_nickname;
+            unset($v['buy_uid']);
+            $v['payeeReferee'] = $member['referee_nickname'];
+        }
+
+        return $result;
     }
 
     public function record($id)
