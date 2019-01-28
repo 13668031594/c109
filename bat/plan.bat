4a21c7cabb5eda4c90cb03ef827103181b219d6c
yangyang <450428985@qq.com>
2019-01-28 18:38:47 +0800

    123

--- bat/plan.bat
+++ bat/plan.bat
@@ -1,5 +1,5 @@
 @echo off  
 
-start “” “C:\Program Files\Internet Explorer\iexplore.exe” “http://jwzs.ythx123.com/plan”
+start 'C:\Program Files\Internet Explorer\iexplore.exe' 'http://jwzs.ythx123.com/plan'
 ping -n 10 127.1>nul
 taskkill /im iexplore.exe /f
