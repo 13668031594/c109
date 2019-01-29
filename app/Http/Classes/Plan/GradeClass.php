e28bfb63b1706a969cf8f80a1e1781303b32519e
yangyang <450428985@qq.com>
2019-01-29 16:32:10 +0800

    123

--- app/Http/Classes/Plan/GradeClass.php
+++ app/Http/Classes/Plan/GradeClass.php
@@ -8,18 +8,21 @@
 
 namespace App\Http\Classes\Plan;
 
+use App\Models\Member\MemberModel;
+
 class GradeClass extends PlanClass
 {
     public function __construct()
     {
         parent::__construct();
 
-        //新的永动会员
+        //时间节点
+        $days = $this->set['typeOld'];
+        $date = date('Y-m-d H:i:s', strtotime('-' . $days . ' day'));
+
+        $model = new MemberModel();
+        $model->where('created_at','<=',$date)->update(['young_grade' => '20']);
     }
 
-    //修改为永动状态
-    private function grad_30()
-    {
 
-    }
 }
