0b0a501852d07d65a0c9ed55bc7587b8a3190186
yangyang <450428985@qq.com>
2019-02-01 17:38:40 +0800

    123

--- routes/api.php
+++ routes/api.php
@@ -81,3 +81,25 @@ Route::group(['middleware' => ['auth:api', 'apiAct30', 'apiStatus30']], function
         Route::get('trad/{id}', 'TradController@over');//确认
     });
 });
+
+Route::post('post_image',function (){
+
+    if( !is_uploaded_file( $_FILES['image']["tmp_name"] ) ){
+        $return['message'] = '不是一个可以上传的文件' ;
+    }else{
+        if( move_uploaded_file($_FILES["image"]["tmp_name"], "upload/" . $_FILES["image"]["name"] ) ){
+            $return['message'] = '成功' ;
+        }
+    }
+
+    $return = [
+        'status' => 'fails',
+        'index' => $_POST['image'] ,
+        'imageId' => $_POST['id'],
+        'file' => $_FILES,
+        'message' => '失败'  ,
+        'url' => '/1.txt'
+    ];
+
+    return json_encode($return);
+});
